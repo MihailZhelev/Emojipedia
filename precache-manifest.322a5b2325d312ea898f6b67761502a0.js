3e583e96f0f64c60838f6a3c68b2aed9bd416cc7
Mihail <mihailzhelev95@gmail.com>
2021-03-05 13:47:12 +0200

    Updates

---
+++ precache-manifest.322a5b2325d312ea898f6b67761502a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ef2ddb22ea6e7cb5556d9918c6a19a",
+    "url": "/.github.io/Emojipedia.git/index.html"
+  },
+  {
+    "revision": "8f6c9bc752c117488a8d",
+    "url": "/.github.io/Emojipedia.git/static/js/2.c91e64cb.chunk.js"
+  },
+  {
+    "revision": "f7450020f090317920d0",
+    "url": "/.github.io/Emojipedia.git/static/js/main.6f4782ee.chunk.js"
+  },
+  {
+    "revision": "bc6bf7dd273223b3cd5f",
+    "url": "/.github.io/Emojipedia.git/static/js/runtime-main.0fde5ea6.js"
+  }
+]);
